3f97ff3301c0f1d25c84d5df49078620e7993e00
Kirill <pro100normo@mail.ru>
2024-10-12 19:16:14 +0700

    gizmo simplified

--- scenes/spawner/gizmo_spawner.gd
+++ scenes/spawner/gizmo_spawner.gd
@@ -2,7 +2,7 @@
 extends Node2D
 
 @export var loading_scene: PackedScene
-@export_range(0.0, 100.0, 0.1)
+@export_range(0.0, 1000.0, 0.1)
 var radius := 10.0
 
 @export_category("Timer Property")
@@ -11,10 +11,29 @@ var wait_time := 1
 @export var one_shot := false
 @export var autostart := false
 
+func _ready() -> void:
+	var timer: Timer = FromUnity.find_node_in_children(self, Timer)
+	if autostart and !Engine.is_editor_hint(): timer.start()
+	timer.wait_time = wait_time
+	timer.one_shot = one_shot
+
+
+var self_x = position.x
+var self_y = position.y
+func _on_timer_timeout() -> void: 
+	var instance = load(loading_scene.resource_path).instantiate()
+	
+	var rand := RandomNumberGenerator.new()
+	var random_x = rand.randf_range(self_x, self_x + radius)
+	var random_y = rand.randf_range(self_y, self_y + radius)
+	
+	instance.position = Vector2(random_x, random_y)
+	add_child(instance)
+
+
 func _process(delta):
 	if Engine.is_editor_hint():
 		queue_redraw()
-
 func _draw() -> void:
 	if Engine.is_editor_hint():
 		draw_circle(Vector2.ZERO, radius, Color.FIREBRICK, false)
--- scenes/spawner/spawner.gd
+++ scenes/spawner/spawner.gd
@@ -1,23 +1,30 @@
 extends Node2D
 
-var parent
+@export var loading_scene: PackedScene
+@export_range(0.0, 1000.0, 0.1)
+var radius := 10.0
+
+@export_category("Timer Property")
+@export_range(0.0, 100.0, 0.1)
+var wait_time := 1
+@export var one_shot := false
+@export var autostart := false
+
 func _ready() -> void:
-	parent = get_parent()
-	
-	var timer: Timer = FromUnity.find_node_in_children(parent, Timer)
-	if parent.autostart: timer.start()
-	timer.wait_time = parent.wait_time
-	timer.one_shot = parent.one_shot
+	var timer: Timer = FromUnity.find_node_in_children(self, Timer)
+	if autostart: timer.start()
+	timer.wait_time = wait_time
+	timer.one_shot = one_shot
 
 
 var self_x = position.x
 var self_y = position.y
 func _on_timer_timeout() -> void: 
-	var instance = load(parent.loading_scene.resource_path).instantiate()
+	var instance = load(loading_scene.resource_path).instantiate()
 	
 	var rand := RandomNumberGenerator.new()
-	var random_x = rand.randf_range(self_x, self_x + parent.radius)
-	var random_y = rand.randf_range(self_y, self_y + parent.radius)
+	var random_x = rand.randf_range(self_x, self_x + radius)
+	var random_y = rand.randf_range(self_y, self_y + radius)
 	
 	instance.position = Vector2(random_x, random_y)
 	add_child(instance)
